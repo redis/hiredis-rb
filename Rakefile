e3b2d6982058d2391b938b50338af7e2ae43d525
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-09-21 12:24:57 +0200

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,16 @@
+require 'rake/extensiontask'
+
+Rake::ExtensionTask.new('redis_ext') do |task|
+  # Pass --with-foo-config args to extconf.rb
+  task.config_options = ARGV[1..-1]
+  task.lib_dir = File.join(*['lib', 'redis_ext'])
+end
+
+namespace :hiredis do
+  task :clean do
+    system("cd vendor/hiredis && make clean")
+  end
+end
+
+# "rake clean" should also clean bundled hiredis
+Rake::Task[:clean].enhance(['hiredis:clean'])
---
+++ ext/redis_ext/extconf.rb
@@ -0,0 +1,19 @@
+require 'mkmf'
+
+def need_header(*args)
+  abort "\n--- #{args.first} is missing\n\n" if !find_header(*args)
+end
+
+def need_library(*args)
+  abort "\n--- lib#{args.first} is missing\n\n" if !find_library(*args)
+end
+
+bundled_hiredis_dir = File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis})
+dir_config('hiredis', bundled_hiredis_dir, bundled_hiredis_dir)
+
+# Compile hiredis when the bundled version can be found
+system("cd #{bundled_hiredis_dir} && make static") if File.directory?(bundled_hiredis_dir)
+
+need_header('hiredis.h')
+need_library('hiredis', 'redisReplyReaderCreate')
+create_makefile('redis_ext/redis_ext')
