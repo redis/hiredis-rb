bf8ebd96cc30c2d930395cf631802833b63c8339
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-09-21 21:18:31 +0200

    Avoid networking in benchmarks; this is code for reply parsing only

--- benchmark/compare.rb
+++ benchmark/compare.rb
@@ -5,84 +5,65 @@
 
 require 'rubygems'
 require 'benchmark'
+require 'stringio'
+require 'redis_ext' # require before redis to avoid auto-patching
 require 'redis'
-require 'redis_ext'
 
-r = Redis.new
-
-# Create 10_000 keys with 100 byte payload
-10_000.times { |i|
-  key = "string%04d" % i
-  r.set(key,key)
-}
-
-# Create 10_000 element list
-10_000.times { |i|
-  r.lpush("list", "string%04d" % i)
-}
-
-def both(b, title, &blk)
-  RedisExt::Gems::Redis.unapply! rescue nil
-  r = Redis.new
-  b.report("ruby:#{title}") { blk.call(r) }
-
-  RedisExt::Gems::Redis.apply! rescue nil
-  r = Redis.new
-  b.report(" ext:#{title}") { blk.call(r) }
+def generate_bulk(size)
+  item = "x" * size
+  "$#{item.size}\r\n#{item}\r\n"
 end
 
-def pipelined_get(b, num)
-  title = "GET"
-  title += (" (pipeline of %d)" % num) if num > 1
-  both(b, title) { |r|
-    10_000.times { |i|
-      if num > 1
-        r.pipelined {
-          num.times { |j|
-            r.get("string%04d" % ((i*num+j) % 10_000))
-          }
-        }
-      else
-        r.get("string%04d" % (i % 10_000))
-      end
+def generate_multi_bulk(length,bulk_size)
+  reply = "*#{length}\r\n"
+  length.times { reply << generate_bulk(bulk_size) }
+  reply
+end
+
+def patched_redis_client_socket(buf)
+  client = Redis::Client.new
+  buffer = StringIO.new(buf)
+  client.instance_variable_set(:@sock, buffer)
+  [client,buffer]
+end
+
+def redis_ext_reader(reply)
+  reader = RedisExt::Reader.new
+end
+
+def both(b, num, reply, title)
+  client, buffer = patched_redis_client_socket(reply)
+  b.report("ruby:#{title}") {
+    num.times {
+      buffer.rewind
+      client.read
+    }
+  }
+
+  reader = RedisExt::Reader.new
+  b.report(" ext:#{title}") {
+    num.times {
+      reader.feed reply
+      reader.gets
     }
   }
 end
 
-def mget(b, num)
-  both(b, "MGET %d keys" % num) { |r|
-    10_000.times { |i|
-      keys = num.times.map { |j| "string%04d" % ((i*num+j) % 10_000) }
-      r.mget(*keys)
-    }
-  }
+def bulk(b, num, size)
+  both(b, num, generate_bulk(size),
+    "%dx bulk (%d bytes)" % [num,size])
 end
 
-def pipelined_lrange(b, size, num)
-  title = "LRANGE 0,%d" % (size-1)
-  title += (" (pipeline of %d)" % num) if num > 1
-  both(b, title) { |r|
-    10_000.times { |i|
-      if num > 1
-        r.pipelined {
-          num.times { |j|
-            r.lrange("list", 0, size-1)
-          }
-        }
-      else
-        r.lrange("list", 0, size-1)
-      end
-    }
-  }
+def multi_bulk(b, num, size, bulk_size)
+  both(b, num, generate_multi_bulk(size,bulk_size),
+    "%dx multi bulk (%d items x %d bytes)" % [num,size,bulk_size])
 end
 
 Benchmark.bm(50) do |b|
-  mget(b,100)
-  mget(b,500)
-  pipelined_get(b,1)
-  pipelined_get(b,10)
-  pipelined_lrange(b,100,1)
-  pipelined_lrange(b,100,10)
-  pipelined_lrange(b,500,1)
-  pipelined_lrange(b,500,10)
+  bulk(b, 100000, 10)
+  bulk(b, 100000, 100)
+  bulk(b, 100000, 1000)
+  multi_bulk(b, 10000, 10, 10)
+  multi_bulk(b, 10000, 100, 10)
+  multi_bulk(b, 10000, 1000, 10)
 end
