09f3af8a5ca97850f71dd9266cfba81c8d93b6d2
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-11-02 22:40:27 +0100

    Remove patcher for redis-rb

--- lib/redis_ext.rb
+++ lib/redis_ext.rb
@@ -1,8 +1,2 @@
 require 'redis_ext/redis_ext'
 require 'redis_ext/version'
-require 'redis_ext/gems/redis'
-
-# Patch Redis::Client automatically.
-if defined?(Redis::Client)
-  RedisExt::Gems::Redis.apply!
-end
