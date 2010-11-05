45b644fb35e8e2cf8c33fd8b5bf5cd2dd942388c
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-11-05 01:00:42 +0100

    Installing on Linux resulted in the wrong output directory for the bundle

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -18,4 +18,4 @@ system("cd #{bundled_hiredis_dir} && make static") if File.directory?(bundled_hi
 
 need_header('hiredis.h')
 need_library('hiredis', 'redisReplyReaderCreate')
-create_makefile('hiredis_ext/hiredis_ext')
+create_makefile('hiredis/hiredis_ext')
