6a6fe30b002b3b8c0ad9d438448c212717755270
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-02-04 10:55:35 +0100

    Show warning when falling back to pure Ruby implementation

--- lib/hiredis/reader.rb
+++ lib/hiredis/reader.rb
@@ -3,6 +3,7 @@ module Hiredis
     require "hiredis/ext/reader"
     Reader = Ext::Reader
   rescue LoadError
+    warn "WARNING: could not load hiredis extension, using (slower) pure Ruby implementation."
     require "hiredis/ruby/reader"
     Reader = Ruby::Reader
   end
