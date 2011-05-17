4f3972f7bca0d981b63998c5838c1f96753bd9b0
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-05-17 10:48:48 +0200

    Always link to bundled hiredis
    Ruby's mkmf cannot be configured to use a local, static library instead
    of a global dynamic one. It tried to link against a version that was
    already installed in /usr/lib or similar, which broke when hiredis was
    upgraded to 0.10 (a few function symbols changed). This change makes
    sure the extension is always linked against the bundled, static version.

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -2,20 +2,16 @@ require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
-def need_header(*args)
-  abort "\n--- #{args.first} is missing\n\n" if !find_header(*args)
+hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
+unless File.directory?(hiredis_dir)
+  STDERR.puts "vendor/hiredis missing, please checkout its submodule..."
+  exit 1
 end
 
-def need_library(*args)
-  abort "\n--- lib#{args.first} is missing\n\n" if !find_library(*args)
-end
+# Make sure hiredis is built...
+system("cd #{hiredis_dir} && make static")
 
-bundled_hiredis_dir = File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis})
-dir_config('hiredis', bundled_hiredis_dir, bundled_hiredis_dir)
-
-# Compile hiredis when the bundled version can be found
-system("cd #{bundled_hiredis_dir} && make static") if File.directory?(bundled_hiredis_dir)
-
-need_header('hiredis.h')
-need_library('hiredis', 'redisReaderCreate')
+# Statically link to hiredis (mkmf can't do this for us)
+$CFLAGS << " -I#{hiredis_dir}"
+$LDFLAGS << " #{hiredis_dir}/libhiredis.a"
 create_makefile('hiredis/ext/hiredis_ext')
