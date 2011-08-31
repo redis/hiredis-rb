7ea9d1475d7967e18457dbebd53507bad4644f79
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-08-31 14:56:11 +0200

    No longer require Redis to be running to run tests

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -3,91 +3,141 @@ require 'hiredis/ext/connection' unless RUBY_PLATFORM =~ /java/
 require 'hiredis/ruby/connection'
 
 module ConnectionTests
+
+  attr_reader :hiredis
+
+  DEFAULT_PORT = 6380
+
+  def listen(port = DEFAULT_PORT)
+    IO.popen("nc -l #{port}", "r+") do |io|
+      sleep 0.05 # Give nc a little time to start listening
+
+      begin
+        yield io
+      ensure
+        hiredis.disconnect if hiredis.connected?
+      end
+    end
+  end
+
   def test_connect_wrong_host
     assert_raise RuntimeError, /can't resolve/i do
-      @conn.connect("nonexisting", 6379)
+      hiredis.connect("nonexisting", 6379)
     end
   end
 
   def test_connect_wrong_port
     assert_raise Errno::ECONNREFUSED do
-      @conn.connect("localhost", 6380)
+      hiredis.connect("localhost", 6380)
     end
   end
 
   def test_connected_tcp
-    assert !@conn.connected?
-    @conn.connect("localhost", 6379)
-    assert @conn.connected?
-    @conn.disconnect
-    assert !@conn.connected?
+    socket = TCPServer.new("127.0.0.1", 6380)
+
+    assert !hiredis.connected?
+    hiredis.connect("127.0.0.1", DEFAULT_PORT)
+    assert hiredis.connected?
+    hiredis.disconnect
+    assert !hiredis.connected?
+  ensure
+    socket.close if socket
   end
 
   def test_connect_unix
-    assert !@conn.connected?
-    @conn.connect_unix("/tmp/redis.sock")
-    assert @conn.connected?
-    @conn.disconnect
-    assert !@conn.connected?
+    path = "/tmp/hiredis-rb-test.sock"
+    File.unlink(path) if File.exist?(path)
+    socket = UNIXServer.new(path)
+
+    assert !hiredis.connected?
+    hiredis.connect_unix(path)
+    assert hiredis.connected?
+    hiredis.disconnect
+    assert !hiredis.connected?
+  ensure
+    socket.close if socket
   end
 
   def test_connect_tcp_with_timeout
     assert_raise Errno::ETIMEDOUT do
-      @conn.connect("1.1.1.1", 59876, 500_000)
+      hiredis.connect("1.1.1.1", 59876, 500_000)
     end
   end
 
   def test_read_when_disconnected
     assert_raise RuntimeError, "not connected" do
-      @conn.read
+      hiredis.read
     end
   end
 
   def test_timeout_when_disconnected
     assert_raise RuntimeError, "not connected" do
-      @conn.timeout = 1
+      hiredis.timeout = 1
     end
   end
 
   def test_wrong_value_for_timeout
-    @conn.connect("localhost", 6379)
-    assert_raise Errno::EDOM do
-      @conn.timeout = -10
+    listen do |_|
+      hiredis.connect("localhost", DEFAULT_PORT)
+
+      assert_raise Errno::EDOM do
+        hiredis.timeout = -10
+      end
     end
   end
 
   def test_read_against_eof
-    @conn.connect("localhost", 6379)
-    @conn.write(["QUIT"])
-    assert_equal "OK", @conn.read
+    listen do |server|
+      hiredis.connect("localhost", 6380)
+      hiredis.write(["QUIT"])
 
-    assert_raise Errno::ECONNRESET do
-      @conn.read
+      # Reply to QUIT and disconnect
+      server.write "+OK\r\n"
+      server.close_write
+
+      # Reply for QUIT can be read
+      assert_equal "OK", hiredis.read
+
+      # Next read should raise
+      assert_raise Errno::ECONNRESET do
+        hiredis.read
+      end
     end
   end
 
   def test_symbol_in_argument_list
-    @conn.connect("localhost", 6379)
-    @conn.write([:info])
-    assert_kind_of String, @conn.read
+    listen do |server|
+      hiredis.connect("localhost", 6380)
+      hiredis.write([:info])
+
+      server.write "$2\r\nhi\r\n"
+
+      assert_kind_of String, hiredis.read
+    end
   end
 
   def test_read_against_timeout
-    @conn.connect("localhost", 6379)
-    @conn.timeout = 10_000
+    listen do |_|
+      hiredis.connect("localhost", DEFAULT_PORT)
+      hiredis.timeout = 10_000
 
-    assert_raise Errno::EAGAIN do
-      @conn.read
+      assert_raise Errno::EAGAIN do
+        hiredis.read
+      end
     end
   end
 
   def test_raise_on_error_reply
-    @conn.connect("localhost", 6379)
-    @conn.write(["GET"])
+    listen do |server|
+      hiredis.connect("localhost", 6380)
+      hiredis.write(["GET"])
 
-    err = @conn.read
-    assert_match /wrong number of arguments/i, err.message
-    assert_kind_of RuntimeError, err
+      server.write "-ERR wrong number of arguments\r\n"
+
+      err = hiredis.read
+      assert_match /wrong number of arguments/i, err.message
+      assert_kind_of RuntimeError, err
+    end
   end
 end
 
@@ -96,7 +146,7 @@ if defined?(Hiredis::Ruby::Connection)
     include ConnectionTests
 
     def setup
-      @conn = Hiredis::Ruby::Connection.new
+      @hiredis = Hiredis::Ruby::Connection.new
     end
   end
 end
@@ -106,7 +156,7 @@ if defined?(Hiredis::Ext::Connection)
     include ConnectionTests
 
     def setup
-      @conn = Hiredis::Ext::Connection.new
+      @hiredis = Hiredis::Ext::Connection.new
     end
   end
 end
