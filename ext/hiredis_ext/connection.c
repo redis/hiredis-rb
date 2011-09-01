fe43d1a3a1a0af83be62328881a8e14fe7d63b5a
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-09-01 10:57:55 +0200

    Refactor extension connection class
    Now it uses non-blocking I/O everywhere, so the Ruby VM can schedule
    other threads while this code is waiting on select(2), while still
    enforcing the configured timeouts. Previously, this was implemented
    using socket timeouts before, which block the entire process.

--- ext/hiredis_ext/connection.c
+++ ext/hiredis_ext/connection.c
@@ -4,13 +4,26 @@
 
 typedef struct redisParentContext {
     redisContext *context;
+    struct timeval *timeout;
 } redisParentContext;
 
-static void parent_context_try_free(redisParentContext *pc) {
+static void parent_context_try_free_context(redisParentContext *pc) {
     if (pc->context) {
         redisFree(pc->context);
         pc->context = NULL;
     }
+}
+
+static void parent_context_try_free_timeout(redisParentContext *pc) {
+    if (pc->timeout) {
+        free(pc->timeout);
+        pc->timeout = NULL;
+    }
+}
+
+static void parent_context_try_free(redisParentContext *pc) {
+    parent_context_try_free_context(pc);
+    parent_context_try_free_timeout(pc);
 }
 
 static void parent_context_mark(redisParentContext *pc) {
@@ -55,20 +68,56 @@ static void parent_context_raise(redisParentContext *pc) {
 static VALUE connection_parent_context_alloc(VALUE klass) {
     redisParentContext *pc = malloc(sizeof(*pc));
     pc->context = NULL;
+    pc->timeout = NULL;
     return Data_Wrap_Struct(klass, parent_context_mark, parent_context_free, pc);
 }
 
-static VALUE connection_generic_connect(VALUE self, redisContext *c) {
+static int __wait_readable(int fd, struct timeval *timeout, int *isset) {
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+
+    if (rb_thread_select(fd + 1, &fds, NULL, NULL, timeout) < 0) {
+        return -1;
+    }
+
+    if (FD_ISSET(fd, &fds) && isset) {
+        *isset = 1;
+    }
+
+    return 0;
+}
+
+static int __wait_writable(int fd, struct timeval *timeout, int *isset) {
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+
+    if (rb_thread_select(fd + 1, NULL, &fds, NULL, timeout) < 0) {
+        return -1;
+    }
+
+    if (FD_ISSET(fd, &fds) && isset) {
+        *isset = 1;
+    }
+
+    return 0;
+}
+
+static VALUE connection_generic_connect(VALUE self, redisContext *c, VALUE arg_timeout) {
     redisParentContext *pc;
-    int err;
-    char errstr[1024];
+    struct timeval tv;
+    struct timeval *timeout = NULL;
 
     Data_Get_Struct(self,redisParentContext,pc);
 
     if (c->err) {
+        char buf[1024];
+        int err;
+
         /* Copy error and free context */
         err = c->err;
-        snprintf(errstr,sizeof(errstr),"%s",c->errstr);
+        snprintf(buf,sizeof(buf),"%s",c->errstr);
         redisFree(c);
 
         if (err == REDIS_ERR_IO) {
@@ -76,86 +125,107 @@ static VALUE connection_generic_connect(VALUE self, redisContext *c) {
             rb_sys_fail(0);
         } else {
             /* Raise something else */
-            rb_raise(rb_eRuntimeError,"%s",errstr);
+            rb_raise(rb_eRuntimeError,"%s",buf);
         }
     }
 
-    c->reader->fn = &redisExtReplyObjectFunctions;
-    pc->context = c;
-    return Qnil;
-}
+    /* Default to context-wide timeout setting */
+    if (pc->timeout != NULL) {
+        timeout = pc->timeout;
+    }
 
-static struct timeval __timeout_from_robj(VALUE usecs) {
-    int s = NUM2INT(usecs)/1000000;
-    int us = NUM2INT(usecs)-(s*1000000);
-    struct timeval timeout = { s, us };
-    return timeout;
+    /* Override timeout when timeout argument is available */
+    if (arg_timeout != Qnil) {
+        tv.tv_sec = NUM2INT(arg_timeout) / 1000000;
+        tv.tv_usec = NUM2INT(arg_timeout) % 1000000;
+        timeout = &tv;
+    }
+
+    /* Wait for socket to become writable */
+    int writable = 0;
+    if (__wait_writable(c->fd, timeout, &writable) < 0) {
+        goto sys_fail;
+    }
+
+    if (!writable) {
+        errno = ETIMEDOUT;
+        goto sys_fail;
+    }
+
+    /* Check for socket error */
+    int optval = 0;
+    socklen_t optlen = sizeof(optval);
+    if (getsockopt(c->fd, SOL_SOCKET, SO_ERROR, &optval, &optlen) < 0) {
+        goto sys_fail;
+    }
+
+    if (optval) {
+        errno = optval;
+        goto sys_fail;
+    }
+
+    parent_context_try_free(pc);
+    pc->context = c;
+    pc->context->reader->fn = &redisExtReplyObjectFunctions;
+    return Qnil;
+
+sys_fail:
+    redisFree(c);
+    rb_sys_fail(0);
 }
 
 static VALUE connection_connect(int argc, VALUE *argv, VALUE self) {
     redisParentContext *pc;
     redisContext *c;
-    VALUE *_host = NULL;
-    VALUE *_port = NULL;
-    VALUE *_timeout = NULL;
-    char *host;
-    int port;
-    struct timeval timeout;
+    VALUE arg_host = Qnil;
+    VALUE arg_port = Qnil;
+    VALUE arg_timeout = Qnil;
 
     if (argc == 2 || argc == 3) {
-        _host = &argv[0];
-        _port = &argv[1];
-        if (argc == 3)
-            _timeout = &argv[2];
+        arg_host = argv[0];
+        arg_port = argv[1];
+
+        if (argc == 3) {
+            arg_timeout = argv[2];
+
+            /* Sanity check */
+            if (NUM2INT(arg_timeout) <= 0) {
+                rb_raise(rb_eArgError, "timeout should be positive");
+            }
+        }
     } else {
         rb_raise(rb_eArgError, "invalid number of arguments");
-        return Qnil;
     }
 
     Data_Get_Struct(self,redisParentContext,pc);
-    parent_context_try_free(pc);
-
-    host = StringValuePtr(*_host);
-    port = NUM2INT(*_port);
-    if (_timeout != NULL) {
-        timeout = __timeout_from_robj(*_timeout);
-        c = redisConnectWithTimeout(host,port,timeout);
-    } else {
-        c = redisConnect(host,port);
-    }
-
-    return connection_generic_connect(self,c);
+    c = redisConnectNonBlock(StringValuePtr(arg_host), NUM2INT(arg_port));
+    return connection_generic_connect(self,c,arg_timeout);
 }
 
 static VALUE connection_connect_unix(int argc, VALUE *argv, VALUE self) {
     redisParentContext *pc;
     redisContext *c;
-    VALUE *_path = NULL;
-    VALUE *_timeout = NULL;
-    char *path;
-    struct timeval timeout;
+    VALUE arg_path = Qnil;
+    VALUE arg_timeout = Qnil;
 
     if (argc == 1 || argc == 2) {
-        _path = &argv[0];
-        if (argc == 2)
-            _timeout = &argv[1];
+        arg_path = argv[0];
+
+        if (argc == 2) {
+            arg_timeout = argv[1];
+
+            /* Sanity check */
+            if (NUM2INT(arg_timeout) <= 0) {
+                rb_raise(rb_eArgError, "timeout should be positive");
+            }
+        }
     } else {
         rb_raise(rb_eArgError, "invalid number of arguments");
-        return Qnil;
     }
 
     Data_Get_Struct(self,redisParentContext,pc);
-    parent_context_try_free(pc);
-
-    path = StringValuePtr(*_path);
-    if (_timeout != NULL) {
-        timeout = __timeout_from_robj(*_timeout);
-        c = redisConnectUnixWithTimeout(path,timeout);
-    } else {
-        c = redisConnectUnix(path);
-    }
-
-    return connection_generic_connect(self,c);
+    c = redisConnectUnixNonBlock(StringValuePtr(arg_path));
+    return connection_generic_connect(self,c,arg_timeout);
 }
 
 static VALUE connection_is_connected(VALUE self) {
@@ -216,25 +286,72 @@ static int __get_reply(redisParentContext *pc, VALUE *reply) {
     void *aux = NULL;
 
     /* Try to read pending replies */
-    if (redisGetReplyFromReader(c,&aux) == REDIS_ERR)
+    if (redisGetReplyFromReader(c,&aux) == REDIS_ERR) {
+        /* Protocol error */
         return -1;
+    }
 
     if (aux == NULL) {
-        do { /* Write until done */
-            if (redisBufferWrite(c,&wdone) == REDIS_ERR)
+        /* Write until the write buffer is drained */
+        while (!wdone) {
+            errno = 0;
+
+            if (redisBufferWrite(c, &wdone) == REDIS_ERR) {
+                /* Socket error */
                 return -1;
-        } while (!wdone);
-        do { /* Read until there is a reply */
-            rb_thread_wait_fd(c->fd);
-            if (redisBufferRead(c) == REDIS_ERR)
+            }
+
+            if (errno == EAGAIN) {
+                int writable = 0;
+
+                if (__wait_writable(c->fd, pc->timeout, &writable) < 0) {
+                    rb_sys_fail(0);
+                }
+
+                if (!writable) {
+                    errno = EAGAIN;
+                    rb_sys_fail(0);
+                }
+            }
+        }
+
+        /* Read until there is a full reply */
+        while (aux == NULL) {
+            errno = 0;
+
+            if (redisBufferRead(c) == REDIS_ERR) {
+                /* Socket error */
                 return -1;
-            if (redisGetReplyFromReader(c,&aux) == REDIS_ERR)
+            }
+
+            if (errno == EAGAIN) {
+                int readable = 0;
+
+                if (__wait_readable(c->fd, pc->timeout, &readable) < 0) {
+                    rb_sys_fail(0);
+                }
+
+                if (!readable) {
+                    errno = EAGAIN;
+                    rb_sys_fail(0);
+                }
+
+                /* Retry */
+                continue;
+            }
+
+            if (redisGetReplyFromReader(c,&aux) == REDIS_ERR) {
+                /* Protocol error */
                 return -1;
-        } while (aux == NULL);
+            }
+        }
     }
 
     /* Set reply object */
-    if (reply != NULL) *reply = (VALUE)aux;
+    if (reply != NULL) {
+        *reply = (VALUE)aux;
+    }
+
     return 0;
 }
 
@@ -254,18 +371,21 @@ static VALUE connection_read(VALUE self) {
 
 static VALUE connection_set_timeout(VALUE self, VALUE usecs) {
     redisParentContext *pc;
-    int s = NUM2INT(usecs)/1000000;
-    int us = NUM2INT(usecs)-(s*1000000);
-    struct timeval timeout = { s, us };
+    struct timeval *ptr;
 
     Data_Get_Struct(self,redisParentContext,pc);
-    if (!pc->context)
-        rb_raise(rb_eRuntimeError, "not connected");
 
-    if (redisSetTimeout(pc->context,timeout) == REDIS_ERR)
-        parent_context_raise(pc);
+    /* Sanity check */
+    if (NUM2INT(usecs) <= 0) {
+        rb_raise(rb_eArgError, "timeout should be positive");
+    }
 
-    return usecs;
+    parent_context_try_free_timeout(pc);
+    ptr = malloc(sizeof(*ptr));
+    ptr->tv_sec = NUM2INT(usecs) / 1000000;
+    ptr->tv_usec = NUM2INT(usecs) % 1000000;
+    pc->timeout = ptr;
+    return Qnil;
 }
 
 
