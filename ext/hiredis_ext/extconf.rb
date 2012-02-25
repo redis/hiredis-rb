936b6dd633618e6ce9f89804228e075dce46692e
Kelley Reynolds <kelley@insidesystems.net>
2012-02-25 16:52:55 -0500

    Fix extconf.rb to use gmake on BSD/Solaris

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+GMAKE       = Config::CONFIG['host_os'].downcase =~ /bsd|solaris/ ? "gmake" : "make"
 
 hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
 unless File.directory?(hiredis_dir)
@@ -9,7 +10,7 @@ unless File.directory?(hiredis_dir)
 end
 
 # Make sure hiredis is built...
-system("cd #{hiredis_dir} && make static")
+system("cd #{hiredis_dir} && #{GMAKE} static")
 
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
