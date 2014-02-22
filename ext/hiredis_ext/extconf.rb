de3fa2f22e83f15dac6c05319d5d27d5143a1843
Adam Stepinski <adam@edmodo.com>
2014-02-21 16:15:53 -0800

    gmake improvements

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -1,20 +1,16 @@
 require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+GMAKE       = Config::CONFIG['host_os'].downcase =~ /bsd|solaris/ ? "gmake" : "make"
 
 hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
 unless File.directory?(hiredis_dir)
   STDERR.puts "vendor/hiredis missing, please checkout its submodule..."
-  exit 1
+    exit 1
 end
 
 # Make sure hiredis is built...
-gnu_make = system("make --version 2>/dev/null | grep 'GNU Make' > /dev/null")
-if gnu_make
-  system("cd #{hiredis_dir} && make static")
-else
-  system("cd #{hiredis_dir} && gmake static")
-end
+system("cd #{hiredis_dir} && #{GMAKE} static")
 
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
