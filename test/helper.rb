b85cc40b75af956e1252a7871cc6a15e397d659c
Jan-Erik Rediger <janerik@fnordig.de>
2015-12-29 15:26:26 +0100

    Force utf-8 in tests
    Seems it's not standard on jruby

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'minitest/autorun'
 require_relative '../lib/hiredis/ext/connection' unless RUBY_PLATFORM =~ /java|mswin|mingw/i
 require_relative '../lib/hiredis/ruby/reader'
